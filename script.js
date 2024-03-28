73eb097b369508194309b4cd6d53912cf26636f2
NaveenKrish757 <naveendeveloper757@gmail.com>
2024-03-28 12:01:34 +0530

    SCRIPT MODIFIED 28-MAR

--- script.js
+++ script.js
@@ -3,23 +3,28 @@ let copybtn = document.getElementById("copy-btn");
 submitbtn.addEventListener("click", collect);
 async function collect() {
   const longUrl = document.getElementById("longurl").value;
+  console.log(longUrl);
   let content = document.getElementsByClassName("content");
   const apiURL = `https://tinyurl.com/api-create.php?url=${encodeURIComponent(
     longUrl
   )}`;
-  try {
+  console.log(!longUrl == "");
+  if (!longUrl == "") {
     await fetch(apiURL)
       .then((res) => res.text())
       .then((tiny) => {
-        if (!content[0].value == "") {
-          content[0].value = tiny;
-        } else {
-          alert(`Please Enter Link First....`);
-        }
+        console.log(tiny);
+        content[0].value = tiny;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred while shortening URL");
       });
-  } catch (error) {}
+  } else {
+    alert("Please enter a URL.");
+    return;
+  }
 }
-
 copybtn.addEventListener("click", () => {
   let content = document.getElementsByClassName("content");
 
